bd026d43498407fed72b9702e612ec8efc91c275
Paolo Contessi <paolo.contessi@gmail.com>
2022-11-26 14:41:56 +0100

    chore: pretty print

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"os"
+
 	"com.github/conteit/uds-101/cmd"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -16,6 +19,8 @@ var (
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	cmd.SetVersion(version, commit, date)
 	cmd.Execute()
 }
